93139939e696f573abb0744e70aa02834b6cc8c8
akrabatt <starwars-1999@yandex.ru>
2025-04-24 01:49:27 +0200

    copyed Daemon class to separate files

---
+++ src/Daemon.cpp
@@ -0,0 +1,23 @@
+#include "../include/Daemon.h"
+
+Daemon::Daemon() {
+    pid_t pid = fork();
+    if (pid < 0) {
+        throw std::runtime_error("Error start daemon");
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    if (setsid() < 0) {
+        throw std::runtime_error("Error create new session");
+    }
+
+    if (chdir("/") != 0) {
+        throw std::runtime_error("Error change work directory");
+    }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
